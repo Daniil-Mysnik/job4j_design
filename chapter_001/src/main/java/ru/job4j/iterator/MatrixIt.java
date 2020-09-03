e2573b7879822c123c83da38c4849e51e77a8526
daniil_mysnik_990 <69dantes6901>
2020-09-04 01:16:52 +0600

    [#270182]

---
+++ chapter_001/src/main/java/ru/job4j/iterator/MatrixIt.java
@@ -0,0 +1,33 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIt implements Iterator<Integer> {
+    private final int[][] data;
+    private int row = 0;
+    private int column = 0;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return row < data.length - 1 || column < data[row].length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        while (column >= data[row].length) {
+            row++;
+            column = 0;
+        }
+        int rsl = data[row][column];
+        column++;
+        return rsl;
+    }
+}
