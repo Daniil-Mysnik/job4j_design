604af2c48f66a264f38ed7886eb8c5b266b2a258
daniil_mysnik_990 <69dantes6901>
2020-09-11 20:31:17 +0600

    [#270211]

---
+++ chapter_002/src/main/java/ru.jo4j.io/LogFilter.java
@@ -0,0 +1,28 @@
+package ru.jo4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            list = in.lines().map(s -> s.split(" "))
+                    .filter(strings -> strings[strings.length - 2].equals("404"))
+                    .map(Arrays::toString)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
