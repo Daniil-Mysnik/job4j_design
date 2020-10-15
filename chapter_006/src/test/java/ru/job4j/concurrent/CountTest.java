97a2b3bae179c319a5dc5be27d9c3849dfeca6a9
daniil_mysnik_990 <69dantes6901>
2020-10-15 17:16:36 +0600

    [#317858]

---
+++ chapter_006/src/test/java/ru/job4j/concurrent/CountTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.concurrent;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CountTest {
+    private class ThreadCount extends Thread {
+        private Count count;
+
+        private ThreadCount(Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        //Создаем счетчик.
+        Count count = new Count();
+        //Создаем нити.
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+        //Запускаем нити.
+        first.start();
+        second.start();
+        //Заставляем главную нить дождаться выполнения наших нитей.
+        first.join();
+        second.join();
+        //Проверяем результат.
+        assertEquals(count.get(), 2);
+
+    }
+}
